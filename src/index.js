92200a639a510dc984f190cedf448cd41bd267d5
Cintia Marques <cintiamarques.mk@gmail.com>
2023-11-19 19:22:25 -0300

    req 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -6,6 +8,15 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+app.get('/talker', async (_request, response) => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, 'talker.json'), 'utf-8');
+    const palestrantes = JSON.parse(data);
+    response.status(HTTP_OK_STATUS).json(palestrantes);
+  } catch (error) {
+    response.status(HTTP_OK_STATUS).json([]);
+  }
+});
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -13,5 +24,4 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
-  
 });
